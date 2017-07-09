e3936e9197abd1cecc81e36d6dbdf67076ea5010
BalaWatermelon <jackson1994y+git@gmail.com>
2017-07-10 03:47:36 +0800

    Connectable client and server

--- serverCanvas.py
+++ serverCanvas.py
@@ -3,7 +3,7 @@ from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout
 from PyQt5.QtGui import QPainter, QColor, QFont
 from PyQt5.QtCore import Qt
 from PyQt5 import QtNetwork, QtCore
-
+import pickle
 HOST = "localhost"
 PORT = 9999
 
@@ -15,11 +15,13 @@ class Server(QMainWindow):
         self.tcpServer = QtNetwork.QTcpServer()
         self.tcpServer.listen(QtNetwork.QHostAddress(HOST), PORT)
         self.tcpServer.newConnection.connect(self.addConnection)
+        self.statusBar
         self.canvas = []
         self.initUi()
 
-    def initUi():
+    def initUi(self):
         self.setGeometry(300, 300, 280, 170)
+        self.setWindowTitle('Server')
         self.show()
 
     def paintEvent(self, e):
@@ -56,7 +58,7 @@ class Server(QMainWindow):
 
             if self.clientConnection.nextBlockSize == 0:
 
-                if self.clientConnection.bytesAvailable() < SIZEOF_UINT32:
+                if self.clientConnection.bytesAvailable() < 4:
                     return
 
                 self.clientConnection.nextBlockSize = stream.readUInt32()
@@ -64,14 +66,20 @@ class Server(QMainWindow):
             if self.clientConnection.bytesAvailable() < self.clientConnection.nextBlockSize:
                 return
 
-            self.Message = stream.readQString()
+            self.Message = stream.readBytes()
             self.clientConnection.nextBlockSize = 0
-            self.emit(QtCore.SIGNAL('DataReceived'))
             self.clientConnection.nextBlockSize = 0
-            print(self.Message)
+            self.canvas += pickle.loads(self.Message)
+            self.update()
 
     def removeConnection(self):
         pass
 
     def socketError(self):
         pass
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mainWindow = Server()
+    sys.exit(app.exec_())
--- socketCanvas.py
+++ socketCanvas.py
@@ -1,7 +1,9 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout
 from PyQt5.QtGui import QPainter, QColor, QFont
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSignal
+from PyQt5 import QtCore, QtNetwork
+import pickle
 
 
 class CanvasApplication(QMainWindow):
@@ -21,19 +23,67 @@ class CanvasWidget(QWidget):
     def __init__(self):
         super().__init__()
         self.canvas = []
+        self.buffer = []
+        self.Clientsocket = QtNetwork.QTcpSocket()
+        # Initialize data IO variables
+        self.nextBlockSize = 0
+        self.request = None
+
+        self.Clientsocket.disconnected.connect(self.serverHasStopped)
         self.initUi()
+
+    def SendData(self):
+
+        TargetSystem = 'localhost'
+        Msg2Send = pickle.dumps(self.buffer)
+        self.buffer = []
+        self.Clientsocket.connectToHost(TargetSystem, 9999)
+        # This is required data to be writing
+        self.Clientsocket.waitForConnected(-1)
+        self.request = QtCore.QByteArray()
+        stream = QtCore.QDataStream(
+            self.request, QtCore.QIODevice.WriteOnly)
+        stream.setVersion(QtCore.QDataStream.Qt_4_2)
+        stream.writeUInt32(0)
+        stream.writeBytes(Msg2Send)
+        stream.device().seek(0)
+        stream.writeUInt32(self.request.size() - 4)
+        self.Clientsocket.write(self.request)
+        self.nextBlockSize = 0
+        self.request = None
+        # .close() is not closing the socket connection, so i changed it this
+        self.Clientsocket.disconnectFromHost()
+
+    def serverHasStopped(self):
+
+        self.Clientsocket.close()
+
+    def serverHasError(self):
+        self.ServerError = self.Clientsocket.errorString()
+        self.emit(QtCore.SIGNAL('SocketError'))
+        self.Clientsocket.close()
+
+    def ShowMessage():
+
+        QtGui.QMessageBox.information(None, "Message", server.Message)
 
     def initUi(self):
         self.setGeometry(300, 300, 280, 170)
+        self.setWindowTitle('happy client')
         self.show()
 
     def mousePressEvent(self, event):
         self.canvas.append((event.pos().x(), event.pos().y()))
+        self.buffer.append((event.pos().x(), event.pos().y()))
         self.update()
 
     def mouseMoveEvent(self, event):
         self.canvas.append((event.pos().x(), event.pos().y()))
+        self.buffer.append((event.pos().x(), event.pos().y()))
         self.update()
+
+    def mouseReleaseEvent(self, e):
+        self.SendData()
 
     def paintEvent(self, e):
         painter = QPainter()
@@ -47,7 +97,7 @@ class CanvasWidget(QWidget):
             painter.drawPoint(dot[0], dot[1])
 
 
-if __name__ is '__main__':
+if __name__ == '__main__':
     app = QApplication(sys.argv)
     mainWindow = CanvasWidget()
     sys.exit(app.exec_())
