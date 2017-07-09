9af3ecb598cb847572b40c8feb877e425e8199e1
BalaWatermelon <jackson1994y+git@gmail.com>
2017-07-10 02:32:48 +0800

    Complete client drawing window

--- socketCanvas.py
+++ socketCanvas.py
@@ -1,18 +1,53 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QVBoxLayout
+from PyQt5.QtGui import QPainter, QColor, QFont
+from PyQt5.QtCore import Qt
 
 
-class MyWidget(QWidget):
+class CanvasApplication(QMainWindow):
     def __init__(self):
-        super().__init__()
-        self.setupUi()
+        super.__init__()
+        self.initUi()
+
+    def initUi(self):
+        myCanvas = CanvasWidget(parent=self)
+        vbox = QVBoxLayout()
+        vbox.addWidget(myCanvas)
+        self.setLayout(vbox)
         self.show()
 
-    def setupUi(self):
-        self.setWindowTitle('Hello World!')
+
+class CanvasWidget(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.canvas = []
+        self.initUi()
+
+    def initUi(self):
+        self.setGeometry(300, 300, 280, 170)
+        self.show()
+
+    def mousePressEvent(self, event):
+        self.canvas.append((event.pos().x(), event.pos().y()))
+        self.update()
+
+    def mouseMoveEvent(self, event):
+        self.canvas.append((event.pos().x(), event.pos().y()))
+        self.update()
+
+    def paintEvent(self, e):
+        painter = QPainter()
+        painter.begin(self)
+        self.drawPoints(painter)
+        painter.end()
+
+    def drawPoints(self, painter, event=None):
+        painter.setPen(Qt.red)
+        for dot in self.canvas:
+            painter.drawPoint(dot[0], dot[1])
 
 
 if __name__ is '__main__':
     app = QApplication(sys.argv)
-    mainWindow = MyWidget()
+    mainWindow = CanvasWidget()
     sys.exit(app.exec_())
